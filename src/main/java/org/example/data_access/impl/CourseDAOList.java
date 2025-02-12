f0f77776365b1055372dbf5b22ae7f0ed52c6d94
Daniel Wennström <daniel@wennstrom.io>
2025-02-12 13:20:20 +0100

    CourseDAOList implemented

--- src/main/java/org/example/data_access/impl/CourseDAOList.java
+++ src/main/java/org/example/data_access/impl/CourseDAOList.java
@@ -6,6 +6,7 @@ import org.example.model.CourseImpl;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class CourseDAOList implements CourseDAO {
@@ -13,31 +14,66 @@ public class CourseDAOList implements CourseDAO {
 
     @Override
     public CourseImpl saveCourse(CourseImpl course) {
-        return course;
+        if (course == null)
+            throw new IllegalArgumentException("Course can't be null");
+
+        CourseImpl existingCourse = findById(course.getId());
+        if (existingCourse == null) {
+            courses.add(course);
+            return course;
+        }
+
+        courses.remove(existingCourse);
+
+        existingCourse.setCourseName(course.getCourseName());
+        existingCourse.setSupervisor(course.getSupervisor());
+        existingCourse.setLectures(course.getLectures());
+        existingCourse.setStudents(course.getStudents());
+        existingCourse.setStartDate(course.getStartDate());
+        existingCourse.setWeekDuration(course.getWeekDuration());
+
+        courses.add(existingCourse);
+
+        return existingCourse;
     }
 
     @Override
     public CourseImpl findById(int id) {
-        return null;
+        return courses.stream()
+                .filter(c -> c.getId() == id)
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public List<CourseImpl> findByName(String name) {
-        return List.of();
+        return courses.stream()
+                .filter(c -> Objects.equals(c.getCourseName(), name))
+                .toList();
     }
 
     @Override
     public List<CourseImpl> findByDate(LocalDate date) {
-        return List.of();
+        return courses.stream()
+                .filter(c -> c.getStartDate() == date)
+                .toList();
     }
 
     @Override
     public List<CourseImpl> findAll() {
-        return List.of();
+        return Set.copyOf(courses);
     }
 
     @Override
     public boolean removeCourse(CourseImpl course) {
-        return false;
+        if (course == null) {
+            throw new IllegalArgumentException("Course can't be null");
+        }
+        if (!courses.contains(course)) {
+            throw new IllegalStateException("Course not found in the list");
+        }
+
+        courses.remove(course);
+        return true;
     }
 }
