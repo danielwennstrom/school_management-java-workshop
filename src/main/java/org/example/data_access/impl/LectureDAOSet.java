40f7239557c5090731e07fce2b026379f13e630c
Daniel Wennström <daniel@wennstrom.io>
2025-02-12 14:05:32 +0100

    LectureDAOSet implemented

--- src/main/java/org/example/data_access/impl/LectureDAOSet.java
+++ src/main/java/org/example/data_access/impl/LectureDAOSet.java
@@ -7,39 +7,82 @@ import org.example.model.TeacherImpl;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class LectureDAOSet implements LectureDAO {
     private static Set<LectureImpl> lectures = new HashSet<>();
+    private static volatile LectureDAOSet instance;
+
+    public static LectureDAOSet getInstance() {
+        if (instance == null) {
+            synchronized (LectureDAOSet.class) {
+                if (instance == null)
+                    instance = new LectureDAOSet();
+            }
+        }
+        return instance;
+    }
 
     @Override
     public LectureImpl saveLecture(LectureImpl lecture) {
-        return null;
+        if (lecture == null)
+            throw new IllegalArgumentException("Course can't be null");
+
+        LectureImpl existingLecture = findById(lecture.getId());
+        if (existingLecture == null) {
+            lectures.add(lecture);
+            return lecture;
+        }
+
+        lectures.remove(existingLecture);
+
+        existingLecture.setLectureName(lecture.getLectureName());
+        existingLecture.setDate(lecture.getDate());
+        existingLecture.setTeachers(lecture.getTeachers());
+
+        lectures.add(existingLecture);
+
+        return existingLecture;
     }
 
     @Override
     public LectureImpl findById(int id) {
-        return null;
+        return lectures.stream().filter(l -> l.getId() == id)
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Collection<LectureImpl> findByTeacher(TeacherImpl teacher) {
-        return List.of();
+        return lectures.stream()
+                .filter(l -> l.getTeachers().contains(teacher))
+                .toList();
     }
 
     @Override
     public Collection<LectureImpl> findByCourse(CourseImpl course) {
-        return List.of();
+        Collection<CourseImpl> courses = CourseDAOSet.getInstance().findAll();
+
+        return courses.stream().filter(c -> c.getId() == course.getId())
+                .findFirst()
+                .map(CourseImpl::getLectures).orElse(null);
     }
 
     @Override
     public Collection<LectureImpl> findAll() {
-        return List.of();
+        return Set.copyOf(lectures);
     }
 
     @Override
     public boolean deleteLecture(LectureImpl lecture) {
-        return false;
+        if (lecture == null) {
+            throw new IllegalArgumentException("Lecture can't be null");
+        }
+        if (!lectures.contains(lecture)) {
+            throw new IllegalStateException("Lecture not found in the list");
+        }
+
+        lectures.remove(lecture);
+        return true;
     }
 }
